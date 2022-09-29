e1c51a8406944396fd362d7ee4314417d4b25334
ALDAR-G7 <ronnyald007@gmail.com>
2022-09-29 11:13:52 -0400

    Clicks terminado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import './App.css';
+import Boton from './components/Boton';
+import Contador from './components/Contador';
+import freeCodeCampLogo from './img/freeCodeCamp-logo.png';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClicks, setNumClicks]=useState(0);
+
+  const sumarClick=()=>{
+    //console.log('CLick');
+    setNumClicks(numClicks+1);
+  };
+  const reiniciarContador=()=>{
+    setNumClicks(0);
+  };
+
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Logo de freeCodeCamp'
+        />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador
+          numClicks={numClicks}
+        />
+        <Boton
+          texto='Click'
+          esBotonDeClick={true}
+          manejarClick={sumarClick}
+        />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador}
+        />
+      </div>
     </div>
   );
 }
